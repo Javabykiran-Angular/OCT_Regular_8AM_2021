3f9a3fc41841dfb534fce149a08f45962b0c47fc
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-11-12 09:25:38 +0530

    Day2

---
+++ Typescript/Day2/demo1.js
@@ -0,0 +1,24 @@
+// console.log("Hello World");
+/* */
+//How to create a variable
+var a1;
+var a_b, a$b;
+var a2;
+// Data Type
+// number(int,float,long,double,short) , string(char,string ''/""), 
+//boolean(true/false), any(number,string,boolean,object) , void(netural data type => it does point anything) 
+// misec null & undefined => basically it is used as value in angular
+var a = 10; //Forward declaration & definition
+// console.log('Value of a is '+a);
+// console.log(`Value of a is ${a} = = = +`);
+var b = 'Sumit';
+// console.log("Value of b is "+b);
+var c;
+c = 2.5;
+// console.log("Value of c is "+c);
+c = "Sumit Raokhande";
+// console.log("Value of c is "+c);
+c = true;
+// console.log("Value of c is "+c);
+var d;
+console.log("Value of d is " + d);
---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,49 @@
+
+// console.log("Hello World");
+/* */
+
+//How to create a variable
+
+var a1;
+var a_b,a$b;
+var a2;
+
+// Data Type
+// number(int,float,long,double,short) , string(char,string ''/""), 
+//boolean(true/false), any(number,string,boolean,object) , void(netural data type => it does point anything) 
+// misec null & undefined => basically it is used as value in angular
+
+var a:number=10;   //Forward declaration & definition
+
+// console.log('Value of a is '+a);
+// console.log(`Value of a is ${a} = = = +`);
+
+var b:string='Sumit';
+// console.log("Value of b is "+b);
+
+var c:any;
+c=2.5;
+// console.log("Value of c is "+c);
+c="Sumit Raokhande";
+// console.log("Value of c is "+c);
+c=true;
+// console.log("Value of c is "+c);
+
+var d:number;
+console.log("Value of d is "+d);
+
+var e;
+
+//Operator
+//Airthematic => +,-,/,*,%
+// Logical => &&,||,!
+// Relational =>!=, ==(It only Checks the value),<=,>=,
+// ===(Strictly/Strongly equality )
+// a===b (it checks data type as well as value) 
+//Ternary => condition?expression1:expression2
+//Bitwise => &,|,>>,<<,!,&,|,!,^,~
+//Assignment => =,+=,-=,*=,/=
+//urnary => ++/-- pre/post a--,--a,++a,a++
+
+
+
