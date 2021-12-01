782102a6461a4d95244a3b25366d618f5e5bf883
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-12-01 09:39:16 +0530

    Day03

---
+++ Angular/Day03/class-binding/class-binding.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-binding',
+  templateUrl: './class-binding.component.html',
+  styleUrls: ['./class-binding.component.css']
+})
+export class ClassBindingComponent implements OnInit {
+
+  myclass:string='success';
+  rating:number=21;
+  isError:boolean=false;
+  isSpecial:boolean=true;
+
+  // jsonObj={
+  //   "success":!this.isError,
+  //   "danger" :this.isError,
+  //   'special':true
+  // }
+
+  jsonObj={
+    "success":!this.isError,
+    "danger" :this.isError,
+    'special':this.isSpecial
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
