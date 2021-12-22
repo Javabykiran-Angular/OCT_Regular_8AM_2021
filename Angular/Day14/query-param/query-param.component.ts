bb8044e78ff6f085e75f49cbe57be658da64ffa5
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-12-22 09:17:22 +0530

    Day14

---
+++ Angular/Day14/query-param/query-param.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-query-param',
+  templateUrl: './query-param.component.html',
+  styleUrls: ['./query-param.component.css']
+})
+export class QueryParamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
